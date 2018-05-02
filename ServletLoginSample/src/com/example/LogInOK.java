d6cf826c6a570bd1a9b05b8e8dac0527f87e6e5e
Hyewon Jo <hyewonjo92@gmail.com>
2018-05-02 20:18:35 +0900

    세션 이용하여 간단하게 회원가입, 로그인, 정보수정, 로그아웃 완료

--- ServletLoginSample/src/com/example/LogInOK.java
+++ ServletLoginSample/src/com/example/LogInOK.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
@@ -8,8 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.mysql.jdbc.Connection;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogInOK
@@ -20,9 +23,8 @@ public class LogInOK extends HttpServlet {
        
 	private Connection connection;
 	private Statement stmt;
-	
-	private String name, id, pw, phone1, phone2, phone3, gender;
-	private String[] hobbys;
+	private ResultSet resultSet;
+	private int i = 1;
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,16 +38,66 @@ public class LogInOK extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		actionDo(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		actionDo(request, response);
 	}
-
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = null;
+		String id = null;
+		String pw = null;
+		String phone1 = null;
+		String phone2 = null;
+		String phone3 = null;
+		String gender = null;
+		
+		id = request.getParameter("id");
+		pw = request.getParameter("pw");
+		
+		String query = "select * from member where id = '" + id + "' and pw = '" + pw + "'";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8", "root", "root");
+			stmt = connection.createStatement();
+			resultSet = stmt.executeQuery(query);
+			
+			if(resultSet.next()) {
+				name = resultSet.getString("name");
+				id = resultSet.getString("id");
+				pw = resultSet.getString("pw");
+				phone1 = resultSet.getString("phone1");
+				phone2 = resultSet.getString("phone2");
+				phone3 = resultSet.getString("phone3");
+				gender = resultSet.getString("gender");
+				
+				HttpSession session = request.getSession();
+				session.setAttribute("name", name);
+				session.setAttribute("id", id);
+				session.setAttribute("pw", pw);
+				
+				response.sendRedirect("loginResult.jsp");
+			}else {
+				response.sendRedirect("login.html");
+			}
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(resultSet != null) resultSet.close();
+				if(stmt != null) stmt.close();
+				if(connection != null) connection.close();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
